5794796020cbd1e1752f0ba15cb16426c6eed78a
Henrique Pagno <henriquepagno@gmail.com>
2020-06-25 21:55:54 -0300

    Listing pokemon

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import InfiniteScroll from 'react-infinite-scroller';
 import { GET_POKEMONS } from '../../graphql/get-pokemons';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  const pokemon = useQuery(GET_POKEMONS, {
-    variables: { first: 10 },
-  }).data;
+  const [quantity, setQuantity] = useState(10);
 
-  console.log(pokemon);
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: quantity },
+  });
 
-  return <></>;
+  // const { data: { pokemons = [], fetchMore } = {} } = useQuery(GET_POKEMONS, {
+  //   variables: { first: 20 },
+  // });
+
+  console.log(pokemons);
+  // console.log(pokemons.length > 0);
+
+  // return (
+  //   <>
+  //     {pokemons && (
+  //       <InfiniteScroll
+  //         data-testid="infinite-scroller"
+  //         hasMore={quantity < 20}
+  //         loader={<p>Loading...</p>}
+  //       >
+  //         {pokemons.map((pokemon) => (
+  //           <p>Pokemon {pokemon.id}</p>
+  //         ))}
+  //       </InfiniteScroll>
+  //     )}
+  //   </>
+  // );
+
+  return (
+    <>
+      {pokemons &&
+        pokemons.map((pokemon) => (
+          <p key={pokemon.id}>Pokemon {pokemon.name}</p>
+        ))}
+    </>
+  );
 }
