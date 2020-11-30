02a49e0a141d5cd7834af94e954bbd9090d9ce32
Henrique Pagno de Lima <henriquepagno@gmail.com>
2020-11-30 20:56:16 -0300

    Changes for deployment

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -5,7 +5,10 @@ const cache = new InMemoryCache();
 
 const apolloClient = new ApolloClient({
   cache,
-  uri: 'http://localhost:5000/',
+  uri:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:5000/'
+      : process.env.REACT_APP_API_URL,
 });
 
 export default apolloClient;
